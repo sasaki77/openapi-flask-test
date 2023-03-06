c17ab3cf673fcea31197364958b6d1041569e581
Shinya Sasaki <shinya.sasaki@kek.jp>
2023-03-06 14:26:31 +0900

    Add handlers

---
+++ server/openapi_server/handlers/__init__.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+
+from openapi_server.handlers.handlers import (
+    get_user_handler,
+    get_users_handler,
+    post_user_handler,
+)
---
+++ server/openapi_server/handlers/handlers.py
@@ -0,0 +1,10 @@
+def get_user_handler(name):
+    return f"do get user handler for {name}"
+
+
+def get_users_handler():
+    return "do get users handler"
+
+
+def post_user_handler():
+    return f"do post user handler for {{name}}"
