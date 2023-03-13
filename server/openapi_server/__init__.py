6dec06b4f91d785203582ddb9d6f2a7821acc1a8
Shinya Sasaki <shinya.sasaki@kek.jp>
2023-03-13 10:08:25 +0900

    Fix config style

--- server/openapi_server/__init__.py
+++ server/openapi_server/__init__.py
@@ -3,13 +3,14 @@ import os
 import connexion
 
 from openapi_server import encoder
-from openapi_server.config import DefaultSettings
 from openapi_server.database.database import init_db
+from openapi_server.config import config
 
 
 def create_app():
     app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.config.from_object('openapi_server.config.DefaultSettings')
+    config_name = os.getenv("FLASK_CONFIGURATION", "default")
+    app.app.config.from_object(config[config_name])
     if "OPENAPISERVER_SETTINGS" in os.environ:
         app.app.config.from_envvar('OPENAPISERVER_SETTINGS')
     app.app.json_encoder = encoder.JSONEncoder
--- server/openapi_server/config.py
+++ server/openapi_server/config.py
@@ -1,3 +1,17 @@
-class DefaultSettings(object):
+class Config(object):
+    Testing = False
+
+
+class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///project.db"
-    SQLALCHEMY_ECHO = True
+
+
+class TestingConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///test.db"
+
+
+config = {
+    "development": "openapi_server.config.DevelopmentConfig",
+    "testing": "openapi_server.config.TestingConfig",
+    "default": "openapi_server.config.DevelopmentConfig",
+}
