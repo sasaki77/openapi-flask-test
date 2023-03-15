596701f8eef8c13b3f5f56884ed2070fb064089c
Shinya Sasaki <shinya.sasaki@kek.jp>
2023-03-15 14:44:45 +0900

    Update init py

--- server/openapi_server/__init__.py
+++ server/openapi_server/__init__.py
@@ -7,9 +7,11 @@ from openapi_server.database.database import init_db
 from openapi_server.config import config
 
 
-def create_app():
+def create_app(config_name_=None):
     app = connexion.App(__name__, specification_dir='./openapi/')
     config_name = os.getenv("FLASK_CONFIGURATION", "default")
+    if config_name_:
+        config_name = config_name_
     app.app.config.from_object(config[config_name])
     if "OPENAPISERVER_SETTINGS" in os.environ:
         app.app.config.from_envvar('OPENAPISERVER_SETTINGS')
